slothdabski/android_device_oneplus_sm8150

sm8150-common: DeviceSettings: Add FPS overlay QS tile
Signed-off-by: AnierinB <anierinb@evolution-x.org>

DeviceSettings/src/org/evolution/device/DeviceSettings/FPSTileService.java
@@ -1,0 +1,65 @@
+/**
+ * Copyright (C) 2020 Omni ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceSettings;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (org.evolution.device.DeviceSettings.FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, org.evolution.device.DeviceSettings.FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+}

slothdabski/android_device_oneplus_sm8150-common

DeviceSettings/src/com/pixeldust/device/DeviceSettings/FPSTileService.java
@@ -1,0 +1,65 @@
+/**
+ * Copyright (C) 2020 Omni ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.device.DeviceSettings;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (com.pixeldust.device.DeviceSettings.FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, com.pixeldust.device.DeviceSettings.FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+}