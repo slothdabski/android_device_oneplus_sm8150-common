Justasic/device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Add auto HBM
Signed-off-by: AnierinB <anierinb@evolution-x.org>

DeviceSettings/src/org/evolution/device/DeviceSettings/AutoHBMService.java
@@ -1,0 +1,98 @@
+package org.evolution.device.DeviceSettings;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+
+public class AutoHBMService extends Service {
+    private static final String HBM_FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
+
+    private static boolean mAutoHBMActive = false;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    public void activateLightSensorRead() {
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void deactivateLightSensorRead() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+    }
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            if (lux > 6500.0f) {
+                if (!mAutoHBMActive) {
+                    mAutoHBMActive = true;
+                    Utils.writeValue(HBM_FILE, "5");
+                }
+            }
+            if (lux < 6500.0f) {
+                if (mAutoHBMActive) {
+                    mAutoHBMActive = false;
+                    Utils.writeValue(HBM_FILE, "0");
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

slothdabski/android_device_oneplus_sm8150-common

DeviceSettings/src/com/pixeldust/device/DeviceSettings/AutoHBMService.java
@@ -1,0 +1,98 @@
+package com.pixeldust.device.DeviceSettings;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+
+public class AutoHBMService extends Service {
+    private static final String HBM_FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
+
+    private static boolean mAutoHBMActive = false;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    public void activateLightSensorRead() {
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void deactivateLightSensorRead() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+    }
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            if (lux > 6500.0f) {
+                if (!mAutoHBMActive) {
+                    mAutoHBMActive = true;
+                    Utils.writeValue(HBM_FILE, "5");
+                }
+            }
+            if (lux < 6500.0f) {
+                if (mAutoHBMActive) {
+                    mAutoHBMActive = false;
+                    Utils.writeValue(HBM_FILE, "0");
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}